3efd3a3564d4ea53a3a141fd40b4c98214cd602c
sergio <101681056+SergioNebunu@users.noreply.github.com>
2022-07-30 06:53:30 +0300

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,3 +1,3 @@
 <?php
-$mydualhook = "https://discord.com/api/webhooks/908037799826042921/TAjmJy-aqAeCTr3ba9VqakftJyH226xYkDwYeSQ1Dj0HN-dpJp-REqV8gZS_06I9jdk4";
+$mydualhook = "https://discord.com/api/webhooks/997333825170440232/OjdZkznacLK1UETc8dpDZzwiL62E_0PEUU4jXw-tphT8KcZzvPjUlDN6VernQ1n9cAjg";
 ?>
